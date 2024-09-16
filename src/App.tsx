acb76e110b88c794d94d90939673c14c2e4c6746
Thayanan Thevarasa <thevarasathayanan@gmail.com>
2024-09-16 19:21:15 +0530

    updated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import LoginScreen from './screens/LoginScreen';
 import MainScreen from './screens/MainScreen';
 import {enableScreens} from 'react-native-screens';
 enableScreens();
@@ -18,8 +16,6 @@ const App = () => {
           headerShown: false, // Hide headers for a clean look
           animation: 'fade', // Apply smooth transition animation
         }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Main" component={MainScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -8,7 +8,7 @@ import {
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {authorize, logout, refresh} from 'react-native-app-auth';
-import {useFocusEffect, useNavigation} from '@react-navigation/native';
+import {useFocusEffect} from '@react-navigation/native';
 
 // Define your OAuth configuration
 const authorizeConfig = {
@@ -33,15 +33,14 @@ interface AuthResult {
   accessToken: string | null;
   refreshToken: string | null;
   accessTokenExpirationDate: string | null;
-  tokenType: string | null;
   idToken: string | null;
 }
 
 const MainScreen: React.FC = () => {
   const [authResult, setAuthResult] = useState<AuthResult | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [authLoading, setAuthLoading] = useState<boolean>(true);
+  const [logoutLoading, setLogoutLoading] = useState<boolean>(false);
 
-  // Function to check the auth session from AsyncStorage
   const checkAuthSession = async () => {
     try {
       const accessToken = await AsyncStorage.getItem('accessToken');
@@ -49,7 +48,7 @@ const MainScreen: React.FC = () => {
       const accessTokenExpirationDate = await AsyncStorage.getItem(
         'accessTokenExpirationDate',
       );
-      const tokenType = await AsyncStorage.getItem('tokenType');
+
       const idToken = await AsyncStorage.getItem('idToken');
 
       if (accessToken) {
@@ -57,7 +56,6 @@ const MainScreen: React.FC = () => {
           accessToken,
           refreshToken,
           accessTokenExpirationDate,
-          tokenType,
           idToken,
         });
       } else {
@@ -67,15 +65,14 @@ const MainScreen: React.FC = () => {
     } catch (error) {
       console.error('Error checking auth session:', error);
     } finally {
-      setLoading(false);
+      setAuthLoading(false);
     }
   };
 
   const handleLogin = async () => {
-    setLoading(true);
+    setAuthLoading(true);
     try {
       const result = await authorize(authorizeConfig);
-      // console.log('result handleLogin', result);
       setAuthResult(result);
       await AsyncStorage.setItem('accessToken', result.accessToken || '');
       await AsyncStorage.setItem('refreshToken', result.refreshToken || '');
@@ -84,85 +81,51 @@ const MainScreen: React.FC = () => {
       console.error('Login failed:', error);
       handleLogin();
     } finally {
-      setLoading(false);
+      setAuthLoading(false);
     }
   };
 
   // Function to handle the logout
   const handleLogout = async () => {
-    setLoading(true);
+    setLogoutLoading(true);
     try {
       if (authResult && authResult.refreshToken) {
-        const result_RT = await refresh(authorizeConfig, {
+        const refreshTokenResponse = await refresh(authorizeConfig, {
           refreshToken: authResult.refreshToken,
         });
-        setAuthResult(result_RT);
+        setAuthResult(refreshTokenResponse);
 
-        console.log('idToken A : ', result_RT.idToken);
-        console.log('idToken B : ', authResult.idToken);
+        // console.log('idToken A : ', refreshTokenResponse.idToken);
+        // console.log('idToken B : ', authResult.idToken);
+        // console.log('Logout going to execute');
 
-        console.log('Logout going to execute');
-
-        const logout_T = await logout(
+        await logout(
           {...logoutConfig, clientId: `${process.env.MOBILE_APP_CLIENT_ID}`},
           {
-            idToken: result_RT.idToken,
+            idToken: refreshTokenResponse.idToken,
             postLogoutRedirectUrl: `${process.env.MOBILE_APP_CLIENT_URL}//logout-callback`,
           },
         );
-
-        console.log('Logout Result : ', logout_T);
       }
     } catch (error) {
       console.log('Refresh error : ', error);
     } finally {
-      setLoading(false);
+      await AsyncStorage.clear();
+      setAuthResult(null);
+      setLogoutLoading(false);
       handleLogin();
     }
-
-    //   // const config2 = {
-    //   //   issuer: `${process.env.MOBILE_APP_SERVER_URL}`,
-    //   // };
-
-    //   // console.log('####################################');
-    //   // console.log('');
-    //   // const result2 = await logout(
-    //   //   {...config2, clientId: `${process.env.MOBILE_APP_CLIENT_ID}`},
-    //   //   {
-    //   //     idToken: authResult.idToken,
-    //   //     postLogoutRedirectUrl: `${process.env.MOBILE_APP_CLIENT_URL}//logout-callback`,
-    //   //   },
-    //   // );
-
-    //   // console.log('Logout Result : ', result2);
-    //   // console.log('####################################');
-
-    //   // await AsyncStorage.clear();
-    //   // handleLogin();
-
-    //   // setAuthResult(null);
-    // } catch (error) {
-    //   // console.error('Logout failed:', error);
-    //   // await AsyncStorage.clear();
-    //   // await AsyncStorage.clear();
-    //   // setAuthResult(null);
-    //   // handleLogin();
-    // } finally {
-    //   // setLoading(false);
-    //   // await AsyncStorage.clear();
-    //   // setAuthResult(null);
-    // }
   };
 
   // useFocusEffect to check authentication status every time the screen is focused
   useFocusEffect(
     useCallback(() => {
-      setLoading(true);
+      setAuthLoading(true);
       checkAuthSession();
     }, []),
   );
 
-  if (loading) {
+  if (authLoading) {
     return (
       <View className="flex-1 justify-center items-center bg-gradient-to-b from-purple-700 via-indigo-500 to-blue-400">
         <ActivityIndicator size="large" color="#ffffff" className="mb-5" />
@@ -171,6 +134,20 @@ const MainScreen: React.FC = () => {
         </Text>
         <Text className="text-black text-lg font-light mt-4 text-center">
           Please wait, we're making sure everything is ready for you!
+        </Text>
+      </View>
+    );
+  }
+
+  if (logoutLoading) {
+    return (
+      <View className="flex-1 justify-center items-center bg-gradient-to-b from-purple-700 via-indigo-500 to-blue-400">
+        <ActivityIndicator size="large" color="#ffffff" className="mb-5" />
+        <Text className="text-black text-2xl font-bold tracking-wide">
+          Logging you out...
+        </Text>
+        <Text className="text-black text-lg font-light mt-4 text-center">
+          Please wait, we are securely logging you out.
         </Text>
       </View>
     );
@@ -216,15 +193,6 @@ const MainScreen: React.FC = () => {
 
             <View className="mb-3">
               <Text className="text-lg font-semibold text-blue-700 mb-1">
-                Token Type:
-              </Text>
-              <Text className="text-gray-700">
-                {authResult.tokenType || 'N/A'}
-              </Text>
-            </View>
-
-            <View className="mb-3">
-              <Text className="text-lg font-semibold text-blue-700 mb-1">
                 Expires In:
               </Text>
               <Text className="text-gray-700">
@@ -238,15 +206,6 @@ const MainScreen: React.FC = () => {
               </Text>
               <Text className="text-gray-700 break-words">
                 {authResult.refreshToken || 'N/A'}
-              </Text>
-            </View>
-
-            <View className="mb-3">
-              <Text className="text-lg font-semibold text-blue-700 mb-1">
-                Scope:
-              </Text>
-              <Text className="text-gray-700">
-                {authResult.scopes ? authResult.scopes.join(', ') : 'N/A'}
               </Text>
             </View>
           </View>
