1805ff51620f079fa4b584a29683aff5eb6bdaba
Thayanan Thevarasa <thevarasathayanan@gmail.com>
2024-09-16 22:09:07 +0530

    updated

--- src/utils/apiConstants.js
+++ src/utils/apiConstants.js
@@ -1,5 +1,5 @@
 export const MOBILE_APP_CLIENT_ID = 'mobile-client';
 export const MOBILE_APP_CLIENT_SECRET = 'secretweb';
-// export const MOBILE_APP_SERVER_URL = 'https://big-readily-cod.ngrok-free.app';
-export const MOBILE_APP_SERVER_URL = 'http://192.168.8.104:9000';
+export const MOBILE_APP_SERVER_URL = 'https://big-readily-cod.ngrok-free.app';
+// export const MOBILE_APP_SERVER_URL = 'http://192.168.8.104:9000';
 export const MOBILE_APP_CLIENT_URL = 'com.myoauthmobile:';
